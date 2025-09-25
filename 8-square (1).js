6cca4ffedbdd0ddb0efade8609bd5794c80d859e
mhmd-7 <mohammedettaleb5@gmail.com>
2025-09-25 19:20:54 +0100

    8-square (1).js

--- 8-square (1).js
+++ 8-square (1).js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+// Get the argument
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
   console.log('Missing size');
-} else if (size > 0) {
+} else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
